80c08a94e06202c078bce73d7065040e01b73e33
Chids Osuala <wd012446@student.reading.ac.uk>
2023-09-13 23:18:16 +0100

    added socket binding and error checking

--- src/server.c
+++ src/server.c
@@ -33,7 +33,7 @@ void *getAddr(struct sockaddr *sa){
 }
 
 int main() {
-    struct addrinfo hints, *res, *p; 
+    struct addrinfo hints, *servinfo, *p; 
     struct sockaddr_storage connInfo; //connections address info
     socklen_t connSize;
     struct sigaction sa;
@@ -41,14 +41,30 @@ int main() {
     int sockfd, newfd; //sockfd is where listening happens, newfd is the connection itself
     int sr;
 
-    memset(&hints, 0, sizeof(hints)); //makes sure hints struct is cleared
+    memset(&hints, 0, sizeof(hints)); 
     hints.ai_family = AF_INET; //ipv4
-    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_socktype = SOCK_STREAM; //type of socket im using
     hints.ai_flags = AI_PASSIVE; //uses my ip
 
-    if((sr = getaddrinfo(NULL, PORT_NUM, &hints, &res)) != 0){ //fills out structs in linked list
+    if((sr = getaddrinfo(NULL, PORT_NUM, &hints, &servinfo)) != 0){ //fills out structs in linked list
         fprintf(stderr, "address info: %s\n", gai_strerror(sr));
         return 1;
+    }
+
+    for(p = servinfo; p!=NULL; p = p->ai_next){ //loops thru results from getaddrinfo and attempts to bind to first compatible one
+        if((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
+            perror("error in setting server socket");
+            continue;
+        }
+        if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1){ //allows reusing port if alr occupied by kernel process
+            perror("error in setting socket option");
+            continue;
+        }
+
+        if(bind(sockfd, p->ai_addr, p->ai_addrlen) == -1){
+            perror("error binding socket");
+            continue;
+        }
     }
 
     sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
