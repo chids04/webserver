429698fed9525fab33d0023f4288383ee8acfffd
Chids Osuala <wd012446@student.reading.ac.uk>
2023-09-14 23:57:47 +0100

    talks to client on specifed port and divided code into seperate functions

--- src/server.c
+++ src/server.c
@@ -12,7 +12,7 @@
 #include <signal.h>
 
 #define BACKLOG 5
-#define PORT_NUM "6969"
+#define PORT_NUM "1023"
 
 void sigchld_handler(int s){
     
@@ -27,49 +27,112 @@ void sigchld_handler(int s){
 void *getAddr(struct sockaddr *sa){
 
     //only working with ipv4 right now, might add ipv6 support in future
-    if(sa->sa_family == AF_INET){
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
+    sa->sa_family = AF_INET;
+    return &(((struct sockaddr_in*)sa)->sin_addr);
+    
 }
 
-int main() {
-    struct addrinfo hints, *servinfo, *p; 
-    struct sockaddr_storage connInfo; //connections address info
-    socklen_t connSize;
-    struct sigaction sa;
-    int yes=1;
-    int sockfd, newfd; //sockfd is where listening happens, newfd is the connection itself
+int createServerSocket(){
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int yes = 1;
     int sr;
 
-    memset(&hints, 0, sizeof(hints)); 
-    hints.ai_family = AF_INET; //ipv4
-    hints.ai_socktype = SOCK_STREAM; //type of socket im using
-    hints.ai_flags = AI_PASSIVE; //uses my ip
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET; // ipv4
+    hints.ai_socktype = SOCK_STREAM; // type of socket im using
+    hints.ai_flags = AI_PASSIVE; // uses my IP
 
-    if((sr = getaddrinfo(NULL, PORT_NUM, &hints, &servinfo)) != 0){ //fills out structs in linked list
+    if ((sr = getaddrinfo(NULL, PORT_NUM, &hints, &servinfo)) != 0) {
         fprintf(stderr, "address info: %s\n", gai_strerror(sr));
-        return 1;
+        return -1;
     }
 
-    for(p = servinfo; p!=NULL; p = p->ai_next){ //loops thru results from getaddrinfo and attempts to bind to first compatible one
-        if((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
             perror("error in setting server socket");
             continue;
         }
-        if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1){ //allows reusing port if alr occupied by kernel process
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
             perror("error in setting socket option");
             continue;
         }
 
-        if(bind(sockfd, p->ai_addr, p->ai_addrlen) == -1){
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
             perror("error binding socket");
             continue;
         }
+
+        break;
     }
 
+    freeaddrinfo(servinfo);
 
-    
-    connSize = sizeof(connInfo);
-    newfd = accept(sockfd, (struct sockaddr *)&connInfo, &connSize);
+    if (p == NULL) {
+        perror("could not bind to a socket");
+        return -1;
+    }
 
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("cant listen on binded socket");
+        return -1;
+    }
+
+    return sockfd;
+}
+
+void handleClientConnection(int sockfd){
+
+    struct sockaddr_storage connInfo;
+    socklen_t connSize;
+    char clientip[INET_ADDRSTRLEN];
+    int newfd;
+
+    while (1) {
+        connSize = sizeof(connInfo);
+        newfd = accept(sockfd, (struct sockaddr*)&connInfo, &connSize);
+        if (newfd == -1) {
+            perror("error accepting socket");
+        }
+
+        inet_ntop(connInfo.ss_family, getAddr((struct sockaddr*)&connInfo), clientip, sizeof(clientip));
+        printf("got connection from: %s\n", clientip);
+
+        if (!fork()) {
+            close(sockfd);
+
+            if (send(newfd, "Hello World!", 13, 0) == -1) {
+                perror("error sending message to client");
+            }
+
+            close(newfd);
+            exit(0);
+        }
+
+        close(newfd);
+    }
+}
+
+
+int main() {
+    int sockfd = createServerSocket();
+    if (sockfd == -1) {
+        exit(1);
+    }
+
+    struct sigaction sa;
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+
+    if (sigaction(SIGCHLD, &sa, NULL)) {
+        perror("sigaction");
+        exit(1);
+    }
+
+    printf("server waiting for connections.....\n");
+
+    handleClientConnection(sockfd);
+
+    return 0;
 }
